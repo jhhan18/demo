5958b29b312dac21e22d012c7211dfe1043fc87a
Han <saithis@hotmail.com>
2018-08-01 14:48:50 +0900

    "get post 설정, 템플릿"

--- src/main/java/com/example/demo/web/WelcomeController.java
+++ src/main/java/com/example/demo/web/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	
-	// /helloworld로 get요청, 리턴에 해당하는 welcome.html 매핑
+	// /helloworld url로부터 오는 get 요청을 수행, 리턴에 해당하는 welcome.html 매핑
 	@GetMapping("/helloworld")	
 	public String welcome(String name, int age, Model model) {
 		System.out.println("name : " + name + " age : " + age);
