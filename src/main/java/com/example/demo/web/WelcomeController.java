524063747608864ec56d372bae1023aebc9f53c2
Han <saithis@hotmail.com>
2018-08-01 10:21:47 +0900

    mustache 코드 적용

---
+++ src/main/java/com/example/demo/web/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class WelcomeController {
+	
+	// /helloworld로 get요청, 리턴에 해당하는 welcome.html 매핑
+	@GetMapping("/helloworld")	
+	public String welcome(String name, int age, Model model) {
+		System.out.println("name : " + name + " age : " + age);
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		return "welcome";
+	}
+	
+}
